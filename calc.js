f7c2f0f819c4f932901f708c7796fe4462b31c09
JP <janphsantiago@gmail.com>
2024-06-09 09:09:10 +0800

    Added an event listener for clear button
    Added an ID for the clear button and added an event listener
    to clear the screen and reset the num1 array.

--- calc.js
+++ calc.js
@@ -1,25 +1,43 @@
 const btns = document.querySelectorAll('.button');
+const equals = document.getElementById("equals");
+const clear = document.getElementById("clear");
 
 let result = "";
-let num1 = 0;
-let num2 = 0;
-
+let num1 = [];
+let num2 = [];
+let operator = null;
 
 btns.forEach(button => {
     button.addEventListener('click', handleClick);
 });
+
 function handleClick(event) {
     let buttonValue = event.target.value;
-    buttonValue = num1;
-    console.log('Button clicked:', num1);
+
+    console.log('Button clicked:', buttonValue);
+    num1+= buttonValue;
+
+
+    const calcScreen = document.getElementById("calcScreen");
+    calcScreen.textContent = num1;
     
+    clear.addEventListener("click", function () {
+        calcScreen.textContent = "";
+        num1 = [];
+    });
 }
+
 
 function operate(a, b, operator){
     if (operator === "+"){
-        return a + b;
+        return num1 + num2;
     }
     else if (operator === '-') {
         return a - b;
     }
+
 }
+
+equals.addEventListener("click", function () {
+    
+});
