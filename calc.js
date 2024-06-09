ad46110778f690b6ffd5c5ea2a25753d36c60160
JP <janphsantiago@gmail.com>
2024-06-09 22:09:03 +0800

    Created an event listener for equal sign.
    Added a separate event listener when equal sign is
    clicked. It contains a conditional statement depends on
    the operator clicked. Performs arithmetic operation that
    passes the value to result.

--- calc.js
+++ calc.js
@@ -1,43 +1,69 @@
 const btns = document.querySelectorAll('.button');
 const equals = document.getElementById("equals");
 const clear = document.getElementById("clear");
+const calcScreen = document.getElementById("calcScreen");
+const calcOpera = document.getElementById("calcOpera");
 
-let result = "";
 let num1 = [];
 let num2 = [];
 let operator = null;
+let result = 0;
 
 btns.forEach(button => {
     button.addEventListener('click', handleClick);
 });
 
+
 function handleClick(event) {
     let buttonValue = event.target.value;
-
-    console.log('Button clicked:', buttonValue);
-    num1+= buttonValue;
-
-
-    const calcScreen = document.getElementById("calcScreen");
-    calcScreen.textContent = num1;
-    
-    clear.addEventListener("click", function () {
-        calcScreen.textContent = "";
-        num1 = [];
-    });
-}
-
-
-function operate(a, b, operator){
-    if (operator === "+"){
-        return num1 + num2;
+   
+    if (!isNaN(buttonValue)) {
+        if (operator === null) {
+            num1 = parseInt(num1.toString() + buttonValue);
+        }
+        else{
+            num2 = parseInt(num2.toString() + buttonValue);
+        }
     }
-    else if (operator === '-') {
-        return a - b;
-    }
+    // If the clicked button is an operator
+    else {
+        if (buttonValue === '+' || buttonValue === '-' || buttonValue === '*' || buttonValue === '%') 
+        {
+            operator = buttonValue;
+            console.log('Operator:', operator);
+        }
 
+    }
+    calcScreen.textContent = buttonValue;
+    calcOpera.textContent = num1 + "" + operator+ "" + num2;
 }
 
 equals.addEventListener("click", function () {
-    
-});
+        if (operator === "+"){
+            result += (num1 + num2);
+            console.log(result);
+        }
+        else if (operator === '-') {
+            result += num1 - num2;
+            console.log(result);
+        }
+        else if (operator === '*') {
+            result += num1 * num2;
+            console.log(result);
+        }
+        else if (operator === '/') {
+            result += num1 / num2;
+            console.log(result);
+        }
+    calcScreen.textContent = result;
+
+    });
+
+    //CLEAR BUTTON
+    clear.addEventListener("click", function () {
+    calcScreen.textContent = "";
+    num1 = "";
+    num2 = "";
+    operator = null;
+    result = 0;
+    });
